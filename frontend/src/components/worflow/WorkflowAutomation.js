e246e79891cb18b0bb6b9c8bc079534a4f1c148b
manish Sahani <manishsahani8601@gmail>
2025-06-17 01:48:23 +0530

    backend url change

--- frontend/src/components/worflow/WorkflowAutomation.js
+++ frontend/src/components/worflow/WorkflowAutomation.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://agentic-ai-or4s.onrender.com';
 
 const WorkflowAutomation = () => {
   const [messages, setMessages] = useState([
